b56c950e7c53da47b626bed5dca9ad99dd92f855
Jayaram1702 <ram999jayaram@msitprogram.net>
2022-02-21 16:34:42 +0530

    done code for constructor

--- Queue.java
+++ Queue.java
@@ -5,10 +5,14 @@ import java.io.*;
 import java.util.*;
 
 class Queue {
+    private Stack<Integer> obj1;
+    private Stack<Integer> obj2;
+    int i=0;
 
 
     public Queue() {
-
+        obj1=new Stack<Integer>();
+        obj2=new Stack<Integer>();
     }
 
     // Push element x to the back of queue.
